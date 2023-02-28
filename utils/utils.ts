04d9634a8eae454cbb57481dc57f066531b79930
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2023-02-28 14:22:09 -0400

    Adding functions and utils

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,3 +1,4 @@
 export const navigationRoutes: string[] = ["work", "archives"];
 
 export const homeProfileImage: string = "https://imgur.com/VAXEwKT.png";
+export const AvatarImage = "https://imgur.com/yaefpD9.png";
