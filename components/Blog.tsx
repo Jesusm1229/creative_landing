569c8ff2bf3f02c8a1c270789faa4d8e179576e1
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2023-03-09 13:13:19 -0400

    Setting new layout for blog

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -22,54 +22,57 @@ export default function Blog({
             initial={animate && "hidden"}
             whileInView={animate ? "visible" : ""}
             viewport={{ once: true }}
-            className="bg-white dark:bg-darkSecondary rounded-2xl p-2 flex flex-col sm:flex-row items-center w-ull sm:w-[95%] mx-auto gap-2 md:gap-7 shadow-md md:shadow-lg"
+            className="relative col-span-2 bg-white p-2 flex flex-col  items-center justify-center w-full sm:w-[95%] mx-auto gap-2 md:gap-7 shadow-md md:shadow-lg"
         >
-            <div className="w-full">
+            <div className="relative inset-0 w-full h-full">
                 <Image
                     title={blog.title}
                     alt={blog.title}
                     src={blog.image}
                     width={1200}
-                    height={630}
+                    height={1200}
                     blurDataURL={blog.image}
                     quality={25}
-                    className="my-auto transition-all duration-300 backdrop-blur-xl rounded-xl"
+                    className="inset-0 m-auto max-w-full w-full h-auto  mb-4 object-cover object-center"
                 />
             </div>
 
-            <div className="flex flex-col w-full h-full px-2 pb-2 mt-2 sm:mt-0 sm:p-1 lg:py-5 md:pr-5">
+            <div className="absolute flex flex-col w-full h-full px-2 pb-2 mt-2 sm:mt-0 sm:p-1 lg:py-5 md:pr-5 items-center justify-center text-center">
                 <Link
                     href={`/blogs/${blog.slug}`}
-                    className="font-bold text-neutral-900 md:text-xl dark:text-neutral-200 hover:underline"
+                    className="rounded-full bg-black px-12 py-6 font-bold text-neutral-100 md:text-xl dark:text-neutral-200 hover:underline"
                 >
                     {blog.title}
                 </Link>
-                <p className="mt-3 text-sm sm:text-xs md:text-sm  text-gray-600 dark:text-[#b5b7ba] line-clamp-3 sm:line-clamp-2 md:line-clamp-4 mb-2">
+                <span className="text-xs text-white">
+                    {getFormattedDate(new Date(blog.date))}
+                </span>
+                <p className="mt-3 text-sm sm:text-xs md:text-sm w-3/5 text-gray-100 dark:text-[#b5b7ba] line-clamp-3 sm:line-clamp-2 md:line-clamp-4 mb-2">
                     {blog.excerpt}
                 </p>
 
                 <div className="flex items-center justify-between mt-auto">
                     <div className="z-10 flex items-center gap-3 font-barlow">
-                        <div className="w-[30px]">
+                        {/*  <div className="w-[30px]">
                             <Image
-                                alt="Jatin Sharma"
+                                alt=""
                                 height={933}
                                 width={933}
                                 src={homeProfileImage}
                                 className="rounded-full !m-0 h-full"
                             />
-                        </div>
+                        </div> */}
                         <div className="flex flex-col">
-                            <Link href="/about" className="text-sm font-bold hover:underline">
-                                Jatin Sharma
-                            </Link>
-                            <span className="text-xs">
+                            {/* <Link href="/about" className="text-sm font-bold hover:underline">
+                                Name
+                            </Link> */}
+                            <span className="text-lg text-white">
                                 {getFormattedDate(new Date(blog.date))}
                             </span>
                         </div>
                     </div>
                     <p className="flex items-center justify-between text-xs font-medium text-gray-500 dark:text-dark-3 md:text-sm">
-                        <span>{blog.readingTime.text}</span>
+                        {/*  <span>{blog.readingTime.text}</span> */}
                     </p>
                 </div>
             </div>
--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -60,7 +60,9 @@ export default function Blogs({ blogs }: { blogs: FrontMatter[] }) {
         </PageTop> */}
 
 
-                <AnimatedDiv
+
+                {/*  SEARCH BAR
+               <AnimatedDiv
                     className="relative group w-0 mx-auto text-slate-400 dark:text-gray-300 bg-white dark:bg-darkSecondary rounded-md"
                     variants={searchBarSlideAnimation}
                 >
@@ -80,7 +82,7 @@ export default function Blogs({ blogs }: { blogs: FrontMatter[] }) {
                     >
                         <RiCloseCircleLine className="w-5 h-5 mr-3" />
                     </button>
-                </AnimatedDiv>
+                </AnimatedDiv> */}
 
                 <section className="relative py-5  flex flex-col gap-2 min-h-[50vh]">
                     <AnimatePresence>
@@ -88,16 +90,16 @@ export default function Blogs({ blogs }: { blogs: FrontMatter[] }) {
                             <>
                                 <AnimatedDiv
                                     variants={FadeContainer}
-                                    className="flex items-center justify-between"
+                                    className="flex items-center justify-center"
                                 >
                                     <motion.h3
                                         variants={popUpFromBottomForText}
-                                        className="text-left font-bold text-2xl sm:text-3xl my-5"
+                                        className="text-center font-bold text-2xl sm:text-3xl my-5"
                                     >
-                                        All Posts ({filteredBlogs.length})
+                                        Archives ({filteredBlogs.length})
                                     </motion.h3>
 
-                                    <div className="flex items-center gap-2">
+                                    {/* <div className="flex items-center gap-2">
                                         <Link href="/blogs/bookmark" legacyBehavior>
                                             <motion.div variants={popUp}>
                                                 <BsBookmark
@@ -115,7 +117,7 @@ export default function Blogs({ blogs }: { blogs: FrontMatter[] }) {
                                                 />
                                             </motion.div>
                                         </Link>
-                                    </div>
+                                    </div> */}
                                 </AnimatedDiv>
 
                                 <AnimatedDiv
