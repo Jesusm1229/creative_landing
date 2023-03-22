24ce68dd02ace1438aa67a1f19aeaed66ee7d579
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2023-03-21 20:24:34 -0400

    ..

--- pages/index.tsx
+++ pages/index.tsx
@@ -74,25 +74,25 @@ export default function Home({ blogs }: { blogs: FrontMatter[] }) {
                 </motion.section>
 
                 <section className=" absolute inset-0 w-full -z-10 h-fit  columns-2 md:columns-3 lg:columns-4 mx-auto overflow-clip">
-                    <img className="column__item mb-4" src="https://source.unsplash.com/random/1" />
-                    <img className="mb-4" src="https://source.unsplash.com/random/2" />
-                    <img className="mb-4" src="https://source.unsplash.com/random/3" />
-                    <img className="mb-4" src="https://source.unsplash.com/random/4" />
-                    <img className="mb-4" src="https://source.unsplash.com/random/5" />
-                    <img className="mb-4" src="https://source.unsplash.com/random/6" />
-                    <img className="mb-4" src="https://source.unsplash.com/random/7" />
-                    <img className="mb-4" src="https://source.unsplash.com/random/8" />
-                    <img className="mb-4" src="https://source.unsplash.com/random/10" />
-                    <img className="mb-4" src="https://source.unsplash.com/random/11" />
-                    <img className="mb-4" src="https://source.unsplash.com/random/12" />
-                    <img className="mb-4" src="https://source.unsplash.com/random/13" />
-                    <img className="mb-4" src="https://source.unsplash.com/random/14" />
-                    <img className="mb-4" src="https://source.unsplash.com/random/15" />
-                    <img className="mb-4" src="https://source.unsplash.com/random/16" />
-                    <img className="mb-4" src="https://source.unsplash.com/random/17" />
-                    <img className="mb-4 overflow-clip" src="https://source.unsplash.com/random/18" />
-                    <img className="mb-4 overflow-clip" src="https://source.unsplash.com/random/19" />
-                    <img className="mb-4 overflow-clip" src="https://source.unsplash.com/random/20" />
+                    <img className="column__item mb-4" src="https://source.unsplash.com/random/1" alt="" />
+                    <img className="mb-4" src="https://source.unsplash.com/random/2" alt="" />
+                    <img className="mb-4" src="https://source.unsplash.com/random/3" alt="" />
+                    <img className="mb-4" src="https://source.unsplash.com/random/4" alt="" />
+                    <img className="mb-4" src="https://source.unsplash.com/random/5" alt="" />
+                    <img className="mb-4" src="https://source.unsplash.com/random/6" alt="" />
+                    <img className="mb-4" src="https://source.unsplash.com/random/7" alt="" />
+                    <img className="mb-4" src="https://source.unsplash.com/random/8" alt="" />
+                    <img className="mb-4" src="https://source.unsplash.com/random/10" alt="" />
+                    <img className="mb-4" src="https://source.unsplash.com/random/11" alt="" />
+                    <img className="mb-4" src="https://source.unsplash.com/random/12" alt="" />
+                    <img className="mb-4" src="https://source.unsplash.com/random/13" alt="" />
+                    <img className="mb-4" src="https://source.unsplash.com/random/14" alt="" />
+                    <img className="mb-4" src="https://source.unsplash.com/random/15" alt="" />
+                    <img className="mb-4" src="https://source.unsplash.com/random/16" alt="" />
+                    <img className="mb-4" src="https://source.unsplash.com/random/17" alt="" />
+                    <img className="mb-4 overflow-clip" src="https://source.unsplash.com/random/18" alt="" />
+                    <img className="mb-4 overflow-clip" src="https://source.unsplash.com/random/19" alt="" />
+                    <img className="mb-4 overflow-clip" src="https://source.unsplash.com/random/20" alt="" />
                 </section>
                 <div className="pt-[1290px]">
                     <BlogSection blogs={blogs} />
